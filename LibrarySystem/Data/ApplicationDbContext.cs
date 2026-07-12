46a577288be6be157321e942c0fbc41845c00c92
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-07-12 19:45:42 +0300

    finish models and start in Data folder

--- LibrarySystem/Data/ApplicationDbContext.cs
+++ LibrarySystem/Data/ApplicationDbContext.cs
@@ -1,9 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using LibrarySystem.Models;
 namespace LibrarySystem.Data
 {
     public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+
+        }
+        //create DbSet for each entity
+        public virtual DbSet <Author> TbAuthors { get; set; }
+        public virtual DbSet<Book> TbBooks { get; set; }
+        public virtual DbSet<BookCopy> TbBooksCopies { get; set; }
+        public virtual DbSet<Category> TbCategories { get; set; }
+        public virtual DbSet<Address> TbAddresses { get; set; }
+        public virtual DbSet<Customer> TbCustomers { get; set; }
+        public virtual DbSet<Employee> TbEmployees { get; set; }
+        public virtual DbSet<Operation> TbOperations { get; set; }
+
+
+
 
     }
 }
---
+++ LibrarySystem/Data/Configurations/BookConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using LibrarySystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LibrarySystem.Data.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+
+        }
+    }
+}
